d403a5515f019f6667f31f560abc46c92d7688fc
neal-o-r <neal@n-o-r.xyz>
2018-04-24 00:55:32 +0100

    map

--- hill_walker.py
+++ hill_walker.py
@@ -27,23 +27,20 @@ def grad(pt, eps=1e-2):
         elev3 = elevation((pt[0], pt[1] + eps))
         elev4 = elevation((pt[0], pt[1] - eps))
 
-        lat_slope = elev1 / elev2 - 1
-        lon_slope = elev3 / elev4 - 1
+        lat_slope = elev1 / elev2
+        lon_slope = elev3 / elev4
 
         return np.asarray((lat_slope, lon_slope))
 
-def descent(f, X=None, lr=0.1, maxiter=100, eps=0.01):
+def descent(f, X=None, lr=0.1, maxiter=1000, eps=0.01):
 
         if X is None:
                 X = get_pt()
 
         i = 1
         delta = 1
-        grad = lambda x: approx_fprime(x, elevation, 1e-3)
-
         path = [list(X) + [elevation(X)]]
         while i < maxiter and delta > eps and elevation(X) >= 0:
-
                 step = grad(X) * lr
                 i += 1
                 delta = np.abs(grad(X-step)).sum()
@@ -52,4 +49,34 @@ def descent(f, X=None, lr=0.1, maxiter=100, eps=0.01):
 
         return path
 
+
+def momentum(f, X=None, lr=0.1, mom=0.1, maxiter=1000, eps=0.01):
+
+        if X is None:
+                X = get_pt()
+        v = np.asarray((0, 0))
+
+        i = 1
+        delta = 1
+        path = [list(X) + [elevation(X)]]
+        while i < maxiter and delta > eps and elevation(X) >= 0:
+                v = mom * v + grad(X) * lr
+                i += 1
+                delta = np.abs(grad(X-v)).sum()
+                X = X - v
+                path.append(list(X) + [elevation(X)])
+
+        return path
+
+def write(path):
+
+        with open('output.csv', 'w') as f:
+                for l in path:
+                        f.write(f'{l[0]}, {l[1]}\n')
+
+
+if __name__ == '__main__':
+
+        path = momentum(elevation, X=pt, lr=0.001, mom=0.5)
+        write(path)
 
